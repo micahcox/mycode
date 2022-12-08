f2a3a57acdc9a7f2f48d21f971e92dd23026e806
micahcox <micahcox@yahoo.com>
2022-12-08 14:59:49 +0000

    SQL Lite and flask labs

---
+++ apisqlite/apisqlite01-noapi.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+""" Author: RZFeeser || Alta3 Research
+Gather data returned by various APIs published on OMDB, and cache in a local SQLite DB
+"""
+
+import json
+import sqlite3
+import requests
+
+# Define the base URL
+OMDBURL = "http://www.omdbapi.com/?"
+
+# search for all movies containing string
+def movielookup(mykey, searchstring):
+    """Interactions with OMDB API
+       mykey = omdb api key
+       searchstring = string to search for"""
+    try:
+        # begin constructing API
+        api = f"{OMDBURL}apikey={mykey}&s={searchstring}"
+
+        ## open URL to return 200 response
+        resp = requests.get(api)
+        ## read the file-like object decode JSON to Python data structure
+        return resp.json()
+    except:
+        return False
+
+def trackmeplease(datatotrack):
+    conn = sqlite3.connect('mymovie.db')
+    try:
+        conn.execute('''CREATE TABLE IF NOT EXISTS MOVIES (TITLE TEXT PRIMARY KEY NOT NULL, YEAR INT  NOT NULL);''')
+
+        # loop through the list of movies that was passed in
+        for data in datatotrack:
+            # in the line below, the ? are examples of "bind vars"
+            # this is best practice, and prevents sql injection attacks
+            # never ever use f-strings or concatenate (+) to build your executions
+            conn.execute("INSERT INTO MOVIES (TITLE,YEAR) VALUES (?,?)",(data.get("Title"), data.get("Year")))
+            conn.commit()
+
+        print("Database operation done")
+        conn.close()
+        return True
+    except:
+        return False
+
+# Read in API key for OMDB
+def harvestkey():
+    with open("/home/student/omdb.key") as apikeyfile:
+        return apikeyfile.read().rstrip("\n") # grab the api key out of omdb.key
+
+def printlocaldb():
+    pass
+    #cursor = conn.execute("SELECT * from MOVIES")
+    #for row in cursor:
+    #    print("MOVIE = ", row[0])
+    #    print("YEAR = ", row[1])
+
+
+def main():
+    test_data = [
+        {
+            "Title": "Star Wars",
+            "Year": "1977"
+        },
+        {
+            "Title": "Jaws",
+            "Year": "1975"
+        } 
+    ]
+    trackmeplease(test_data)
+
+if __name__ == "__main__":
+    main()
